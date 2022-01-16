d9f2d7ca279de2018ec0674a25a49fcbcb63587b
Alex Klementovich <a13k13m@gmail.com>
2022-01-16 09:30:44 -0500

    Main update

--- main.cpp
+++ main.cpp
@@ -2,16 +2,13 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <iostream>
+#include "ImageProcessing.h"
 using namespace cv;
+
 int main()
 {
     std::string image_path = samples::findFile("Mt_Washington1.png");
-    Mat img = imread(image_path, IMREAD_COLOR);
-    if (img.empty())
-    {
-        std::cout << "Could not read the image: " << image_path << std::endl;
-        return 1;
-    }
+    Mat img = ImageProc::openImage(image_path);
     imshow("Display window", img);
     int k = waitKey(0); 
     return 0;
